560f6490cddcdf49696112cfc205d20a01136b5a
Connor Denihan <188690869+cdenihan@users.noreply.github.com>
2026-03-24 08:11:46 -0400

    update to sync with Bot-1 repo

--- src/main.cpp
+++ src/main.cpp
@@ -58,10 +58,10 @@ int main()
 {
     // Setup Drivetrain
     std::cout << "[INIT]" << " " << "WELCOME TO PROJECT-XBOT" << std::endl;
-    std::cout << "Library versions:" << KIPR_VERSION << std::endl;
+    std::cout << "Library versions: " << KIPR_VERSION << std::endl;
     DriveTrain DriveTrain(0, 1, 2, 3, 0, 1);
 
-    DriveTrain.SetPerformance(1.0, 1.0, 0.9, 0.9);
+    DriveTrain.SetPerformance(1.0, 1.0, 1.00, 1.00);
     DriveTrain.SetLineTrackingThresholds(200, 200, 2000, 2000);
 
     // Setup Arm
@@ -71,18 +71,113 @@ int main()
     DriveTrain.DriveForward(750, 1500);
     DriveTrain.RotateRight(TICKS_PER_90, 1000);
     DriveTrain.DriveBackward(3000, 1500);
-    DriveTrain.DriveLeftToLine(500);
 
-    DriveTrain.DriveRight(1000, 1000);
-    DriveTrain.DriveForward(1000, 1000);
-    DriveTrain.DriveLeftToLine(500);
+    // Change me
+    DriveTrain.DriveForward(750, 1000);
 
-    // DriveTrain.DriveLeft(3500, 1500);
-    // DriveTrain.DriveForwardToLine(500);
-    // DriveTrain.DriveForwardToLine(500);
+    DriveTrain.StrafeLeftOnToLine(500);
 
-    // DriveTrain.DriveForward(2000, 1000);
-    // DriveTrain.DriveRight(5500, 1500);
+    DriveTrain.StrafeRight(1550, 1000);
+    DriveTrain.DriveForward(1500, 1000);
+    DriveTrain.StrafeLeftOnToLine(500);
 
+    DriveTrain.StrafeRight(1850, 1000);
+    DriveTrain.DriveForwardToLine(500);
+    DriveTrain.DriveBackward(150, 500);
+
+    DriveTrain.RotateLeft(TICKS_PER_90, 1000);
+    // DriveTrain.StrafeRight(25, 1000);
+    DriveTrain.DriveForwardToLine(500);
+
+    DriveTrain.DriveForward(1450, 1000);
+    // DriveTrain.StrafeRight(1400, 1000);
+
+    DriveTrain.StrafeLeft(2750, 1500);
+
+    // Pushing Blue Poms
+    DriveTrain.StrafeRight(850, 1000);
+    DriveTrain.DriveForward(775, 1000);
+    // DriveTrain.SquareWithLine(100);
+    DriveTrain.DriveForward(2000, 1000);
+    DriveTrain.StrafeLeft(500, 1500);
+
+    DriveTrain.DriveBackwardToLine(1000);
+    DriveTrain.DriveBackward(500, 1500);
+    DriveTrain.SquareWithLine(250);
+    DriveTrain.DriveBackward(225, 1000);
+    DriveTrain.StrafeRight(2500, 1500);
+
+    DriveTrain.DriveForward(750, 1000);
+    DriveTrain.DriveBackward(750, 1000);
+
+    DriveTrain.RotateLeft(TICKS_PER_90 / 3, 1000);
+    DriveTrain.RotateRight(TICKS_PER_90 / 3, 1000);
+
+    DriveTrain.DriveForward(400, 1500);
+
+    DriveTrain.RotateRight(TICKS_PER_90 / 3, 1000);
+    DriveTrain.RotateLeft(TICKS_PER_90 / 3, 1000);
+
+    // DriveTrain.DriveBackward(175, 1500);
+
+    DriveTrain.SquareWithLine(250);
+    DriveTrain.DriveBackward(225, 1000);
+    DriveTrain.StrafeRight(2750, 1500);
+
+    DriveTrain.StrafeLeft(100, 1000);
+    DriveTrain.SquareWithLine(250);
+    DriveTrain.DriveForward(350, 1000);
+    DriveTrain.DriveBackwardToLine(500);
+    DriveTrain.DriveBackward(600, 1000);
+    DriveTrain.StrafeRight(5250, 1500);
+
+    DriveTrain.DriveBackward(650, 1000);
+
+    DriveTrain.RotateLeft(TICKS_PER_45, 1000);
+    DriveTrain.DriveBackward(350, 1000);
+    DriveTrain.RotateLeft(TICKS_PER_45, 1000);
+    DriveTrain.DriveBackward(150, 1000);
+
+    DriveTrain.StrafeRightOnToLine(500);
+
+    DriveTrain.StrafeRight(250, 1000);
+    Arm.SetPosition(DownPositionOpen);
+
+    // DriveTrain.DriveBackward(550, 1500);
+
+    DriveTrain.DriveForward(1400, 1500);
+    DriveTrain.DriveForwardToLine(500);
+    // DriveTrain.DriveForwardLineTracking(1500, 500);
+
+    Arm.SetPosition(DownPositionClosed);
+    msleep(1000);
+    Arm.SetPosition(UpPositionClosed);
+
+    DriveTrain.StrafeLeft(500, 1000);
+    DriveTrain.DriveBackward(750, 1500);
+    DriveTrain.StrafeRight(750, 1000);
+    DriveTrain.DriveForward(2000, 1500);
+    DriveTrain.StrafeLeftOnToLine(500);
+    DriveTrain.DriveBackward(750, 1500);
+    DriveTrain.DriveForwardToLine(500);
+
+    DriveTrain.StrafeLeft(500, 1000);
+    DriveTrain.DriveForward(7500, 1500);
+    DriveTrain.StrafeLeft(4000, 1500);
+
+    Arm.SetPosition(DownPositionClosed);
+    msleep(1000);
+    Arm.SetPosition(DownPositionOpen);
+    msleep(1000);
+    Arm.SetPosition(UpPositionOpen);
+
+    DriveTrain.StrafeRight(750, 1500);
+    DriveTrain.RotateRight(TICKS_PER_90, 1000);
+    DriveTrain.DriveForwardToLine(500);
+    DriveTrain.DriveBackward(50, 1500);
+    DriveTrain.StrafeRightToLine(500);
+    DriveTrain.SquareWithLine(250);
+
+    Arm.SetPosition(UpPositionOpen);
     return 0;
 }
