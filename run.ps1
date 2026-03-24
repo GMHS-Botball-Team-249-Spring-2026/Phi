8f9d56e4f2be1cf442a14cc0cde8aa6b0d7928bd
Connor Denihan <188690869+cdenihan@users.noreply.github.com>
2026-03-23 21:47:55 -0400

    feat: add scripts for uploading and running botball user program

---
+++ run.ps1
@@ -0,0 +1,3 @@
+$RobotIP = "192.168.137.82"
+
+ssh kipr@$RobotIP '"/home/kipr/Documents/KISS/Default User/Project XBOT/bin/botball_user_program"'
---
+++ upload.ps1
@@ -0,0 +1,6 @@
+$RobotIP = "192.168.137.82"
+
+
+scp zig-out/bin/botball_user_program "kipr@${RobotIP}:/home/kipr/"
+ssh kipr@$RobotIP 'sudo mv /home/kipr/botball_user_program "/home/kipr/Documents/KISS/Default User/Project XBOT/bin/botball_user_program"'
+ssh kipr@$RobotIP 'sudo chmod +x "/home/kipr/Documents/KISS/Default User/Project XBOT/bin/botball_user_program"'
