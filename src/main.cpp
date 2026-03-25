0240394bc00f32e858fd2a5d9b630be9fbf22156
Connor Denihan <188690869+cdenihan@users.noreply.github.com>
2026-03-25 12:22:49 -0400

    Enhance main.cpp with improved comments and refined robot movement logic for better clarity and functionality

--- src/main.cpp
+++ src/main.cpp
@@ -47,43 +47,40 @@ int main() {
   std::cout << "Library versions: " << KIPR_VERSION << std::endl;
   DriveTrain DriveTrain(0, 1, 2, 3, 0, 1);
 
+  // Set performance and line tracking thresholds
   DriveTrain.SetPerformance(1.0, 1.0, 1.00, 1.00);
   DriveTrain.SetLineTrackingThresholds(200, 200, 2000, 2000);
 
   // Setup Arm
   Arm Arm(0, 1, 2);
+  // Set arm position to open
   Arm.SetPosition(UpPositionOpen);
 
+  // Rotate & Drive into side
   DriveTrain.DriveForward(750, 1500);
   DriveTrain.RotateRight(TICKS_PER_90, 1000);
   DriveTrain.DriveBackward(3000, 1500);
 
-  // Change me
+
+  // Push startbox poms
   DriveTrain.DriveForward(750, 1000);
-
   DriveTrain.StrafeLeftOnToLine(500);
-
   DriveTrain.StrafeRight(1550, 1000);
   DriveTrain.DriveForward(1500, 1000);
   DriveTrain.StrafeLeftOnToLine(500);
-
   DriveTrain.StrafeRight(1850, 1000);
   DriveTrain.DriveForwardToLine(500);
   DriveTrain.DriveBackward(150, 500);
 
+  // Rotate and Drive left to push poms
   DriveTrain.RotateLeft(TICKS_PER_90, 1000);
-  // DriveTrain.StrafeRight(25, 1000);
   DriveTrain.DriveForwardToLine(500);
-
   DriveTrain.DriveForward(1450, 1000);
-  // DriveTrain.StrafeRight(1400, 1000);
-
   DriveTrain.StrafeLeft(2750, 1500);
 
-  // Pushing Blue Poms
+  // Get blue poms
   DriveTrain.StrafeRight(850, 1000);
   DriveTrain.DriveForward(775, 1000);
-  // DriveTrain.SquareWithLine(100);
   DriveTrain.DriveForward(2000, 1000);
   DriveTrain.StrafeLeft(500, 1500);
 
@@ -91,6 +88,8 @@ int main() {
   DriveTrain.DriveBackward(500, 1500);
   DriveTrain.SquareWithLine(250);
   DriveTrain.DriveBackward(225, 1000);
+
+  // Start pushing my orange poms
   DriveTrain.StrafeRight(2500, 1500);
 
   DriveTrain.DriveForward(750, 1000);
@@ -103,8 +102,6 @@ int main() {
 
   DriveTrain.RotateRight(TICKS_PER_90 / 3, 1000);
   DriveTrain.RotateLeft(TICKS_PER_90 / 3, 1000);
-
-  // DriveTrain.DriveBackward(175, 1500);
 
   DriveTrain.SquareWithLine(250);
   DriveTrain.DriveBackward(225, 1000);
@@ -119,25 +116,24 @@ int main() {
 
   DriveTrain.DriveBackward(650, 1000);
 
+
+  // Move poms to be strafed into another box
   DriveTrain.RotateLeft(TICKS_PER_45, 1000);
   DriveTrain.DriveBackward(400, 1000);
   DriveTrain.RotateLeft(TICKS_PER_45, 1000);
   DriveTrain.DriveBackward(150, 1000);
-
   DriveTrain.StrafeRightOnToLine(500);
-
   DriveTrain.StrafeRight(250, 1000);
+
+  // Get traffic cone
   Arm.SetPosition(DownPositionOpen);
-
-  // DriveTrain.DriveBackward(550, 1500);
-
   DriveTrain.DriveForward(1400, 1500);
   DriveTrain.DriveForwardToLine(500);
-
   Arm.SetPosition(DownPositionClosed);
   msleep(1000);
   Arm.SetPosition(UpPositionClosed);
 
+  // Push cube, and RTB
   DriveTrain.DriveForward(1000, 1500);
   DriveTrain.StrafeLeft(500, 1500);
   DriveTrain.RotateRight(TICKS_PER_90, 1500);
@@ -145,18 +141,14 @@ int main() {
   DriveTrain.DriveBackwardToLine(1000);
   DriveTrain.DriveBackward(450, 1500);
   DriveTrain.SquareWithLine(250);
-
   DriveTrain.StrafeLeft(5500, 1500);
-
   DriveTrain.DriveBackward(3000, 1500);
-
   DriveTrain.RotateLeft(TICKS_PER_90, 1500);
   DriveTrain.StrafeLeft(1000, 1500);
 
+  // Drop traffic cone
   Arm.SetPosition(DownPositionOpen);
   msleep(1000);
   Arm.SetPosition(UpPositionOpen);
-
-
   return 0;
 }
